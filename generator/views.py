b7e599b492ae3505bf18f67fb22051836f109721
fly <fly@gmail.com>
2022-10-05 22:26:27 +0800

    index css 更改

--- generator/views.py
+++ generator/views.py
@@ -1,14 +1,14 @@
-from http.client import HTTPResponse
-import random
+from django.http import HttpResponse
 from django.shortcuts import render
+
+import random
 
 # Create your views here.
 
 
 def password(request):
-
+    # a~z
     chars = [chr(c) for c in range(97, 123)]
-    print(chars)
     # None ==>False
     if request.GET.get('uppercase'):
         chars += [chr(c).upper() for c in range(97, 123)]
@@ -18,17 +18,18 @@ def password(request):
 
     if request.GET.get('special'):
         chars += list('@#$%^&*')
-    print(chars)
 
+    print(chars)
     # 取得輸入的長度
     length = eval(request.GET.get('length')) if request.GET.get('input-length') == '' \
         else eval(request.GET.get('input-length'))
 
-    #print(random.sample(chars, length))
-
+    # 樣本數(不會重複，但不能取樣超過容器大小)
+    # print(random.sample(chars,length))
     password = ''.join([random.choice(chars) for i in range(length)])
-    print(password)
 
+    # 變成字串?
+    print(password)
     print(length)
 
     return render(request, './password.html', {'password': password})
@@ -37,4 +38,4 @@ def password(request):
 def index(request):
     print('Hello Django')
 
-    return render(request, 'index.html',)
+    return render(request, './index.html')
